f7bb92da0472b4055b09d06ef45fc2150decf1c8
kpnava <kpnava.dev@gmail.com>
2016-02-11 13:31:47 +0800

    Commit

--- client/views/router.js
+++ client/views/router.js
@@ -3,3 +3,45 @@ Router.route('/view-employees-kanban', function(){
 
 	this.render('view_employees_kanban')
 })
+
+Router.route('/expenses', function(){
+	this.layout('layout'),
+
+	this.render('expenses')
+})
+
+Router.route('/appraisal', function(){
+	this.layout('layout'),
+
+	this.render('appraisal')
+})
+
+Router.route('/interview-request', function(){
+	this.layout('layout'),
+
+	this.render('interview-request')
+})
+
+Router.route('/employee-tags', function(){
+	this.layout('layout'),
+
+	this.render('employee-tags')
+})
+
+Router.route('/departments', function(){
+	this.layout('layout'),
+
+	this.render('departments')
+})
+
+Router.route('/appraisal-plans', function(){
+	this.layout('layout'),
+
+	this.render('appraisal-plans')
+})
+
+Router.route('/expense-categories', function(){
+	this.layout('layout'),
+
+	this.render('expense-categories')
+})
